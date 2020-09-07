b43250ef91eb10502f6a030001b5e98e3c2a896a
Nicolas Guerrero <stephenguerrero43@gmail.com>
2020-09-06 22:10:44 -0500

    0.1.29

---
+++ README.md
@@ -0,0 +1,16 @@
+## Eth-Lib
+
+Lightweight Ethereum libraries. 
+
+Based on the work by [maiavictor](https://github.com/maiavictor)
+
+## Usage
+
+```javascript
+import {
+  API,
+  Account,
+  Bytes,
+  // etc
+} from "https://deno.land/x/npm_eth_lib@0.1.29/mod.js";
+```
---
+++ deps.js
@@ -0,0 +1,8 @@
+export { default as Buffer } from "https://cdn.skypack.dev/buffer@5.6.0";
+export { default as elliptic } from "https://cdn.skypack.dev/elliptic@6.5.3";
+export { default as BN } from "https://cdn.skypack.dev/bn.js@5.1.3";
+export { default as ws } from "https://cdn.skypack.dev/ws@7.3.1";
+export * as ethjs_account from "https://deno.land/x/npm_ethjs_account@0.1.5/mod.js";
+export * as ethjs_signer from "https://deno.land/x/npm_ethjs_signer@0.1.1/mod.js";
+export * as rlp from "https://deno.land/x/npm_rlp@2.2.6/mod.ts";
+export * as testing from "https://deno.land/std@0.68.0/testing/asserts.ts";
